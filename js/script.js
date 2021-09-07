4ae34fdeccd4791d5bc821e29e78897901e46b1c
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-07 12:57:25 +0300

    It will remove the previous class and set a new bg color

--- js/script.js
+++ js/script.js
@@ -26,12 +26,15 @@ $('h5').click(function(){
 
 
 $("button#green").click(function(){
+    $("body").removeClass();
     $("body").addClass("green-background")
 });
 $("button#yellow").click(function(){
+    $("body").removeClass();
     $("body").addClass("yellow-background")
 });
 $("button#red").click(function(){
+    $("body").removeClass();
     $("body").addClass("red-background")
 });
 })
