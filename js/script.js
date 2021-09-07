acfdcf00efbe69fa0ed5e5a97570b05b9662c96d
Annaliskirwa <annaliskirwa@gmail.com>
2021-09-07 13:58:29 +0300

    It will add an alert message and background color to the prepended lists

--- js/script.js
+++ js/script.js
@@ -42,15 +42,21 @@ $("button#red").click(function(){
 $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $('li').css('background-color', 'green');
   });
 
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $('li').css('background-color', 'blue');
   });
 
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $('li').css('background-color', 'crimson');
+    $("li").click(function(){
+        alert("Hi")
+    })
   });
 })
